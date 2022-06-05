da3e83a5845f3ead439bb528f36779c9812f1f07
BorislavaGrigorova <105098582+BorislavaGrigorova@users.noreply.github.com>
2022-06-05 15:44:56 +0300

    Add file with tests for Summator

---
+++ Summator.Tests/SummatorTests.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+
+namespace Summator.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test_Sum_TwoPossiviveNumbers()
+        {
+            long actual = Summator.Sum(new int[] { 5, 7 });
+
+            long expected = 12;
+
+            Assert.That(expected == actual);
+        }
+
+        [Test]
+        public void Test_Sum_OnePossitiveNumber()
+        {
+            long actual = Summator.Sum(new int[] { 5 });
+
+            Assert.That(actual == 5);
+        }
+
+        [Test]
+        public void Test_Sum_TwoNegativeNumbers()
+        {
+            long actual = Summator.Sum(new int[] { -7, -9 });
+
+            Assert.That(actual == -16);
+        }
+
+        [Test]
+        public void Test_Sum_EmtyArray()
+        {
+            long actual = Summator.Sum(new int[] { });
+
+            Assert.That(actual == 0);
+        }
+
+        [Test]
+        public void Test_Sum_BigValues()
+        {
+            long actual = Summator.Sum(new int[] { 2000000000, 2000000000, 2000000000, 2000000000 });
+
+            Assert.AreEqual(8000000000, actual);
+
+        }
+
+        [Test]
+        public void Test_Average_TwoPossiviveNumbers()
+        {
+            int actual = Summator.Average(new int[] { 5, 7 });
+
+            int expected = 6;
+
+            Assert.That(expected == actual);
+        }
+    }
+}
